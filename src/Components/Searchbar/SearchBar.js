4fab112ba8b8048a3efedcefe3d679104504c8b0
juanwalls <juancparedesg@gmail.com>
2022-04-01 11:51:20 -0700

    no busca llamadas

---
+++ src/Components/Searchbar/SearchBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './SearchBar.css';
+export class SearchBar extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+
+    handleTermChange(event){
+        this.setState({term: event.target.value});
+    }
+
+    search(){
+        this.props.onSearch(this.state.term);
+    }
+    render(){
+        return (
+    <div className="SearchBar">
+    <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} />
+    <button className="SearchButton">SEARCH</button>
+    </div>
+        );
+    }
+}
